060eaeed23d1f5c5836d193e3a8bb6772b4c5697
Nguyên Vũ Văn <hoangnguyenvuive210698@gmail.com>
2019-04-10 08:58:40 +0700

    xong trang 58

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -23,7 +23,8 @@ namespace BigSchool.Controllers
         {
             var viewModel = new CourseViewModel
             {
-                Categories = _dbContext.Categories.ToList()
+                Categories = _dbContext.Categories.ToList(),
+                Heading = "Add Course"
             };
             return View(viewModel);
         }
@@ -48,6 +49,7 @@ namespace BigSchool.Controllers
             _dbContext.SaveChanges();
             return RedirectToAction("Index", "Home");
         }
+        //Tham gia khoa hoc
         [Authorize]
         public ActionResult Attending()
         {
@@ -64,6 +66,75 @@ namespace BigSchool.Controllers
                 ShowAction = User.Identity.IsAuthenticated
             };
             return View(viewModel);
+        }
+        //Theo doi giang vien
+        public ActionResult Following()
+        {
+            var userId = User.Identity.GetUserId();
+            var courses = _dbContext.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Course)
+                .Include(l => l.Lecturer)
+                .Include(l => l.Category)
+                .ToList();
+            var viewModel = new CourseViewModel
+            {
+                UpcommingCourses = courses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
+        }
+        //Danh sach duoc user tao
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var courses = _dbContext.Courses
+                .Where(c => c.LecturerId == userId && c.DateTime > DateTime.Now)
+                .Include(l => l.Lecturer)
+                .Include(c => c.Category)
+                .ToList();
+            return View(courses);
+        }
+
+        //Ham Edit 
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList(),
+                Date = course.DateTime.ToString("dd/MM/yyyy"),
+                Time = course.DateTime.ToString("HH:mm"),
+                Category = course.CategoryId,
+                Place = course.Place,
+                Heading = "Edit Course",
+                Id = course.Id
+            };
+            return View("Create", viewModel);
+        }
+        //Ham Update
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == viewModel.Id && c.LecturerId == userId);
+
+            course.Place = viewModel.Place;
+            course.DateTime = viewModel.GetDateTime();
+            course.CategoryId = viewModel.Category;
+
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- BigSchool/ViewModels/CourseViewModel.cs
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -9,23 +9,32 @@ namespace BigSchool.ViewModels
 {
     public class CourseViewModel
     {
+        public int Id { get; set; }
+        
         public IEnumerable<Course> UpcommingCourses { get; set; }
         public bool ShowAction { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Địa điểm -- không được để trống")]
         public string Place { get; set; }
 
-        [Required]
-        [FutureDate]
+        [Required(ErrorMessage = "Ngày -- không được để trống")]
+        [FutureDate(ErrorMessage = "Ngày tháng năm phải lớn hơn ngày tháng năm ở hiện tại")]
         public string Date { get; set; }
 
-        [Required]
-        [ValidTime]
+        [Required(ErrorMessage = "Thời gian -- không được để trống")]
+        [ValidTime(ErrorMessage = "Định dạng sai (hh:mm)")]
         public string Time { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Vui lòng chọn -- Thể loại")]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
+
+        public string Heading { get; set; }
+        public string Action
+        {
+            get { return (Id != 0) ? "Update" : "Create"; }
+        }
+
         public DateTime GetDateTime()
         {
             return DateTime.Parse(string.Format("{0} {1}", Date, Time));
